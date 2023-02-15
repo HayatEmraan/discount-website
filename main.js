b88c18b7cc2238aeed4ab565b297838dea9b654b
Emraan Hasan Hayat <emraanhasanhayat@gmail.com>
2023-02-15 14:21:25 +0600

    discountValue

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 document.getElementById('discountButton').addEventListener('click', function(){
     const withoutDiscount = document.getElementById('withoutDiscount');
     const withoutDiscountField = parseFloat(withoutDiscount.innerText);
-    withoutDiscount.innerText = withoutDiscountField - (withoutDiscountField*30)/100;
+    const discountField = document.getElementById('discountNumber');
+    const discountFieldValue = discountField.value;
+    if(discountFieldValue === 'DICS30'){
+        withoutDiscount.innerText = withoutDiscountField - (withoutDiscountField*30)/100;
+    }
 });
